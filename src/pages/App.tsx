b0cb1ec8eb492794f10b5102118f552d01cb8ae4
David Castner <davidjcastner@gmail.com>
2026-03-11 06:35:35 -0400

    add hash router
    hash router is best option for working with github pages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
 
 const App: React.FC = () => {
     return (
-        <main className='app'>
-            <div className='app-content'>
-                <h1>David Castner</h1>
-            </div>
-        </main>
+        <HashRouter>
+            <main className='app'>
+                <div className='app-content'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                    </Routes>
+                </div>
+            </main>
+        </HashRouter>
     );
 };
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+const Home: React.FC = () => {
+    return (
+        <h1>David Castner</h1>
+    );
+};
+
+export default Home;
