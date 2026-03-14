881d41b36032c47e86a85e06f016d7fc5a48c0ab
David Castner <davidjcastner@gmail.com>
2026-03-14 07:47:08 -0400

    create outline for coding page

--- src/pages/Coding.tsx
+++ src/pages/Coding.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import './Coding.css';
 
 const Coding: React.FC = () => {
-    return (
+    return (<>
         <h1>Coding</h1>
+        <h2>Inheritance Problem</h2>
+        <h2>Shut The Box</h2>
+        <h2>Wordle App and Solver</h2>
+        <h2>Nonogram Solver</h2>
+        <h2>Starcraft 2 AI</h2>
+        <h2>Factorio Compression Mod</h2>
+        <h2>NPM Router</h2></>
     );
 };
 
